fc8d92f3932323a474a4a57c550a2496a8aa3732
Sergei Sivtsev <sergykt@gmail.com>
2024-01-10 16:16:15 +0600

    removed preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { type FC, lazy, Suspense } from 'react';
+import { type FC, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Preloader from '@components/Preloader';
 
 const PageNotFound = lazy(() => import('@pages/PageNotFound'));
 const StarterPage = lazy(() => import('@pages/StarterPage'));
 const EmulatorPage = lazy(() => import('@pages/EmulatorPage'));
 
 const App: FC = () => (
-  <Suspense fallback={<Preloader />}>
-    <Routes>
-      <Route path='/*' element={<PageNotFound />} />
-      <Route path='/' element={<StarterPage />} />
-      <Route path='/emulator/*' element={<EmulatorPage />} />
-    </Routes>
-  </Suspense>
+  <Routes>
+    <Route path='/*' element={<PageNotFound />} />
+    <Route path='/' element={<StarterPage />} />
+    <Route path='/emulator/*' element={<EmulatorPage />} />
+  </Routes>
 );
 
 export default App;
