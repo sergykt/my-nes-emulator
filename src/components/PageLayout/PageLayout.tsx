c899869241ed687b8ba1e4893e24fe186593d207
Sergei Sivtsev <sergykt@gmail.com>
2024-01-10 16:45:59 +0600

    fixed preloader

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,8 +1,7 @@
-import { type FC, type ComponentProps, Suspense } from 'react';
+import { type FC, type ComponentProps } from 'react';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 import ScrollButton from '@components/ScrollButton';
-import Preloader from '@components/Preloader';
 import styles from './PageLayout.module.scss';
 
 type PageLayoutProps = ComponentProps<'div'>;
@@ -10,9 +9,7 @@ type PageLayoutProps = ComponentProps<'div'>;
 const PageLayout: FC<PageLayoutProps> = ({ children }) => (
   <div className={styles.wrapper}>
     <Header />
-    <main className={styles.main}>
-      <Suspense fallback={<Preloader />}>{children}</Suspense>
-    </main>
+    <main className={styles.main}>{children}</main>
     <Footer />
     <ScrollButton />
   </div>
