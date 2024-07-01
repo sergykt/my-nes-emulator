41980b51f139465a77932289e031204403ab51dd
Sergei Sivtsev <sergykt@gmail.com>
2024-07-01 13:35:34 +0500

    improve: useDebounce improving

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,46 +1,65 @@
-import { useCallback, useRef, type MutableRefObject } from 'react';
+import { useCallback, useEffect, useRef, type MutableRefObject } from 'react';
 import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { type RootState, type AppDispatch } from '../store';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export const useDebounce = (callback: (...args: unknown[]) => void, delay: number) => {
-  const timer = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>;
+export const useLatest = <T>(value: T): { readonly current: T } => {
+  const ref = useRef(value);
+  ref.current = value;
+  return ref;
+};
+
+export const useDebounce = <T>(callback: (...args: T[]) => void, delay: number) => {
+  const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>;
+  const callbackLatest = useLatest(callback);
+
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, []);
 
   return useCallback(
-    (...args: unknown[]) => {
-      if (timer.current) {
-        clearTimeout(timer.current);
+    (...args: T[]) => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
       }
-      timer.current = setTimeout(() => {
-        callback(...args);
+      timerRef.current = setTimeout(() => {
+        callbackLatest.current(...args);
       }, delay);
     },
     [callback, delay]
   );
 };
 
-export const useThrottle = (callback: (...args: unknown[]) => void, delay: number) => {
+export const useThrottle = <T>(callback: (...args: T[]) => void, delay: number) => {
   const throttleRef = useRef(false);
+  const callbackLatest = useLatest(callback);
+  const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, []);
 
   return useCallback(
-    (...args: unknown[]) => {
+    (...args: T[]) => {
       if (!throttleRef.current) {
-        callback(...args);
+        callbackLatest.current(...args);
         throttleRef.current = true;
 
-        setTimeout(() => {
+        timerRef.current = setTimeout(() => {
           throttleRef.current = false;
         }, delay);
       }
     },
     [callback, delay]
   );
-};
-
-export const useLatest = <T>(value: T): { readonly current: T } => {
-  const ref = useRef(value);
-  ref.current = value;
-  return ref;
 };
