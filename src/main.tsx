fd31f4cc0dea13ffbe9fa9d0388475fbc22f7354
Sergei Sivtsev <sergykt@gmail.com>
2025-04-28 21:49:06 +0500

    feat: pwa service-worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,18 @@ import App from './App';
 import './normalize.css';
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
